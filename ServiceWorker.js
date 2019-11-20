23551bb601dbe05487cc42ac19d6d5d9b4fe3d88
Sagona Loïc <loic.sagona@gmail.com>
2019-11-20 16:06:37 +0100

    a

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,38 +1,91 @@
-var cacheName = 'js13kPWA-v1';
-var appShellFiles = [
-  	'./js/app.js',
-	'./js/DecoderWorker.js',
-	'./js/exif.js',
-	'./js/job.js',
-	'./images/icon-cart.png',
-	'./images/icon-setup.png',
-	'./images/icon-transmit.png',
-	'./images/logo.png'
+var CACHE_NAME = 'scanettePWA-v1';
+
+var contentToCache = [
+  './index.html',    
+  './scanettePWA.webmanifest',    
+  './style.css', 
+  './produits.csv', 
+  './js/app.js', 
+  './js/DecoderWorker.js', 
+  './js/exif.js', 
+  './js/job.js', 
+  './images/logo.png', 
+  './images/barcode-scanner.png', 
+  './images/icon-cart.png', 
+  './images/icon-setup.png', 
+  './images/icon-transmit.png', 
+  './favicon.ico',
+  './icons/icon-32.png',
+  './icons/icon-64.png',
+  './icons/icon-96.png',
+  './icons/icon-128.png',
+  './icons/icon-168.png',
+  './icons/icon-192.png',
+  './icons/icon-256.png',
+  './icons/icon-512.png'
 ];
-self.addEventListener('install', (e) => {
 
-	
+var updatableContent = ['index.html', 'style.css', 'app.js', 'produits.csv'];
+
+// service worker installation
+self.addEventListener('install', function(e) {
     console.log('[Service Worker] Install');
-
-	e.waitUntil(
-		caches.open(cacheName).then((cache) => {
-			  console.log('[Service Worker] Caching all: app shell and content');
-		  return cache.addAll(appShellFiles);
-		})
-	  );
+    e.waitUntil(caches.open(CACHE_NAME).then(function(cache) {
+        console.log('[Service Worker] Caching application content & data');
+        return cache.addAll(contentToCache);
+    }));
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((r) => {
-          console.log('[Service Worker] Fetching resource: '+e.request.url);
-      return r || fetch(e.request).then((response) => {
-                return caches.open(cacheName).then((cache) => {
-          console.log('[Service Worker] Caching new resource: '+e.request.url);
-          cache.put(e.request, response.clone());
-          return response;
-        });
-      });
-    })
-  );
+// fecthing data
+self.addEventListener('fetch', function(evt) {
+
+    var fichier = evt.request.url.substr(evt.request.url.lastIndexOf('/')+1);
+    
+    // if requested on an updatable content, load it from the network and cache it
+    if (updatableContent.some(function(uc) { return evt.request.url.indexOf(uc) >= 0; })) {
+        evt.respondWith(
+            caches.open(CACHE_NAME).then(function(cache) {
+                console.log("[Service worker] Trying to fetch from network", fichier);
+                return fetch(evt.request)
+                    .then(function (response) {
+                    // If the response was OK, clone it and store it in the cache.
+                    if (response.status === 200) {
+                        console.log("[Service worker] --> Network available, caching latest version", fichier);
+                        cache.put(evt.request.url, response.clone());
+                    }
+                    return response;
+                }).catch(function (err) {
+                    // Network request failed, try to get it from the cache.
+                    console.log("[Service worker] --> Network unavailable, using cached version", fichier);
+                    return cache.match(evt.request);
+                });
+            }));
+        return;
+    }
+    
+    // otherwise load from cache by default, or fetch it if not present (and update cache)
+    console.log("[Service worker] --> Loading from cache (if present)", fichier);
+    evt.respondWith(
+        caches.open(CACHE_NAME).then(function(cache) {
+            return cache.match(evt.request)
+                .then(function(response) {
+                    return response || fetch(evt.request);
+            })
+        })
+    );
+});
+
+
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        // cleaning previous caches
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                if(CACHE_NAME.indexOf(key) === -1) {
+                    console.log("[Service Worker] Cleaning old cache");
+                    return caches.delete(key);
+                }
+          }));
+        })
+    );
 });
